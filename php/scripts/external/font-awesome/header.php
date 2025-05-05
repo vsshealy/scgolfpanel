143917d0017db703d171d486f27302aac874f36e
Scott Shealy <vsshealy@gmail.com>
2025-05-04 20:11:47 -0400

    Added theme PHP files

---
+++ php/scripts/external/font-awesome/header.php
@@ -0,0 +1,11 @@
+<?php 
+    /**
+     * php/scripts/external/font-awesome/header.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2025.05.04)
+     * @copyright 2025 (2025.05.04)
+    **/
+?>
+
+<script src="https://kit.fontawesome.com/cf6277ea30.js" crossorigin="anonymous"></script>
