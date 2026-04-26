3c6cdbf4a6b1f180e2da157ba5f7c85d7587b134
Scott Shealy <vsshealy@gmail.com>
2026-04-26 13:53:22 -0400

    Added theme PHP files

---
+++ php/scripts/external/bootstrap/header.php
@@ -0,0 +1,11 @@
+<?php 
+    /**
+     * php/scripts/external/bootstrap/header.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2026.01.01)
+     * @copyright 2026 (2026.01.01)
+    **/
+?>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
