88bcbf4988bcd44bda88311448bb687ad2f5037c
Scott Shealy <vsshealy@gmail.com>
2026-06-10 21:10:25 -0400

    Added theme PHP files

---
+++ php/menus/menu-footer-primary.php
@@ -0,0 +1,18 @@
+<?php 
+    /**
+     * php/menus/menu-footer-primary.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0 (2026.01.01)
+     * @copyright 2026 (2026.01.01)
+    **/
+?>
+
+<?php 
+    wp_nav_menu(
+        array(
+            'theme_location' => 'footer-primary',
+            'menu_id' => 'Footer-Primary'
+        )
+    );
+?>
